2e758c4cb15236bac4a950dfdfdfa8fa4ee0414e
felipecidade94 <felipecidade94@gmail.com>
2026-04-08 08:47:53 -0300

    chore: generate build artifacts and assembly information for Ritmo.Api

--- Ritmo.Api/obj/Debug/net8.0/Ritmo.Api.AssemblyInfo.cs
+++ Ritmo.Api/obj/Debug/net8.0/Ritmo.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ritmo.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5107460bd20a9ede25fe34bcd7e04559dc62bf3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c488f13f82b1ceed0b5da01fc3470339286da96b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ritmo.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ritmo.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
